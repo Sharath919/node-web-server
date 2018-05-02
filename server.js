c1bffa32a7a2b6ebc739dd0b31b005e89f14c8cd
Srinadh <sharathchand19141@gmail.com>
2018-05-02 10:52:43 +0530

    setup start scripts port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 let app = express();
+const port = process.env.PORT || 3000 ;
 
 app.set('view engine','hbs');
 hbs.registerPartials(__dirname+'/views/partials');
@@ -30,4 +31,6 @@ app.get('/about',(req,res)=>{
     });
 })
 
-app.listen(3000);
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+});
